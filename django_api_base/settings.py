8a3602f1f6087153325c05a375ad0fd7f1e182ca
Ozgur <ozgur.akcali@yahoo.com>
2019-04-24 22:54:26 +0300

    updated database settings to read values from environment variables

--- django_api_base/settings.py
+++ django_api_base/settings.py
@@ -96,8 +96,12 @@ WSGI_APPLICATION = 'django_api_base.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('dbname'),
+        'USER': os.environ.get('dbuser'),
+        'PASSWORD': os.environ.get('dbpass'),
+        'HOST': os.environ.get('dbhost'),
+        'PORT': os.environ.get('dbport')
     }
 }
 
